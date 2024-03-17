cb9f88ade596fafe2915addc796773d799643871
dam-bal <d.balandowski@icloud.com>
2024-03-17 22:07:58 +0000

    customers migration

--- database/migrations/2024_03_17_220211_create_customers_table.php
+++ database/migrations/2024_03_17_220211_create_customers_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->uuid('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->foreignIdFor(\App\Models\User::class);
             $table->timestamps();
         });
     }
