acecf1fdb380fa247c452d5652b58eec943073d4
dam-bal <d.balandowski@icloud.com>
2024-03-17 22:28:15 +0000

    seeders

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,12 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $product = new Product();
+
+        $product->name = "Product 1";
+        $product->price = 123.0;
+        $product->sku = "PRODUCT-1";
+
+        $product->save();
     }
 }
