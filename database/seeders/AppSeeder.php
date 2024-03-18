d12118a2f71fbd7262ec48e043b39097a2d07eab
dam-bal <d.balandowski@icloud.com>
2024-03-18 18:25:38 +0000

    seeders cleanup

--- database/seeders/AppSeeder.php
+++ database/seeders/AppSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class CartItemSeeder extends Seeder
+class AppSeeder extends Seeder
 {
     /**
      * Run the database seeds.
