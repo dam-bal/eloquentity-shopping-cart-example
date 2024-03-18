22c01d2e2a1aa403329591c249f378fa9e3ab343
dam-bal <d.balandowski@icloud.com>
2024-03-18 20:10:17 +0000

    seeder

--- database/seeders/AppSeeder.php
+++ database/seeders/AppSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Customer;
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class AppSeeder extends Seeder
 {
@@ -12,6 +16,36 @@ class AppSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $product1 = new Product();
+
+        $product1->name = "Product 1";
+        $product1->sku = "PRODUCT-1";
+        $product1->price = 15.0;
+
+        $product1->save();
+
+        $product2 = new Product();
+
+        $product2->name = "Product 1";
+        $product2->sku = "PRODUCT-1";
+        $product2->price = 15.0;
+
+        $product2->save();
+
+        $user = new User();
+
+        $user->name = "User";
+        $user->email = "user@app.com";
+        $user->password = Hash::make('pass123');
+
+        $user->save();
+
+        $customer = new Customer();
+
+        $customer->firstName = "Test";
+        $customer->lastName = "Testing";
+        $customer->userId = $user->id;
+
+        $customer->save();
     }
 }
