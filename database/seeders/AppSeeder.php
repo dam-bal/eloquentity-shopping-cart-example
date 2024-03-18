8d2a1329df4078399abe65af5226edd8085dc745
dam-bal <d.balandowski@icloud.com>
2024-03-18 20:58:33 +0000

    seeder updates

--- database/seeders/AppSeeder.php
+++ database/seeders/AppSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Cart;
+use App\Models\CartItem;
 use App\Models\Customer;
 use App\Models\Product;
 use App\Models\User;
@@ -27,9 +28,9 @@ class AppSeeder extends Seeder
 
         $product2 = new Product();
 
-        $product2->name = "Product 1";
-        $product2->sku = "PRODUCT-1";
-        $product2->price = 15.0;
+        $product2->name = "Product 2";
+        $product2->sku = "PRODUCT-2";
+        $product2->price = 100.0;
 
         $product2->save();
 
@@ -43,16 +44,32 @@ class AppSeeder extends Seeder
 
         $customer = new Customer();
 
-        $customer->firstName = "Test";
-        $customer->lastName = "Testing";
-        $customer->userId = $user->id;
+        $customer->first_name = "Test";
+        $customer->last_name = "Testing";
+        $customer->user_id = $user->id;
 
         $customer->save();
 
         $cart = new Cart();
 
-        $cart->customerId = $customer->id;
+        $cart->customer_id = $customer->id;
 
         $cart->save();
+
+        $cartItem1 = new CartItem();
+
+        $cartItem1->cart_id = $cart->id;
+        $cartItem1->product_id = $product1->id;
+        $cartItem1->quantity = 2;
+
+        $cartItem1->save();
+
+        $cartItem2 = new CartItem();
+
+        $cartItem2->cart_id = $cart->id;
+        $cartItem2->product_id = $product2->id;
+        $cartItem2->quantity = 1;
+
+        $cartItem2->save();
     }
 }
