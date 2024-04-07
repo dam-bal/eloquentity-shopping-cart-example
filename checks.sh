e9ce0e20ffe37130016b89110a9976142d8ff130
dam-bal <d.balandowski@icloud.com>
2024-04-07 08:46:05 +0100

    improvements to check.sh

--- checks.sh
+++ checks.sh
@@ -1,81 +1,40 @@
 #!/usr/bin/env sh
 
+run_command() {
+    temp_file=$(mktemp)
+    $1 >"$temp_file" 2>&1
+
+    if [ $? -eq 0 ]; then
+        echo -e "\e[32mSUCCESS\e[0m"
+        rm "$temp_file"  # Clean up the temporary file on success
+    else
+        echo -e "\e[31mFAIL\e[0m"
+        cat "$temp_file"  # Display the output on failure
+        rm "$temp_file"  # Clean up the temporary file
+    fi
+}
+
 echo "#########"
 echo "# Tests #"
 echo "#########"
-temp_file=$(mktemp)
-
-./vendor/bin/sail artisan test >"$temp_file" 2>&1
-
-if [ $? -eq 0 ]; then
-    echo "SUCCESS"
-    rm "$temp_file"  # Clean up the temporary file on success
-else
-    echo "FAIL"
-    cat "$temp_file"  # Display the output on failure
-    rm "$temp_file"  # Clean up the temporary file
-fi
+run_command "./vendor/bin/sail artisan test"
 
 echo "#######################"
 echo "# Architecture Checks #"
 echo "#######################"
-temp_file=$(mktemp)
-
-./vendor/bin/sail php vendor/bin/deptrac >"$temp_file" 2>&1
-
-if [ $? -eq 0 ]; then
-    echo "SUCCESS"
-    rm "$temp_file"  # Clean up the temporary file on success
-else
-    echo "FAIL"
-    cat "$temp_file"  # Display the output on failure
-    rm "$temp_file"  # Clean up the temporary file
-fi
+run_command "./vendor/bin/sail php vendor/bin/deptrac"
 
 echo "###################"
 echo "# Static Analysis #"
 echo "###################"
-temp_file=$(mktemp)
-
-./vendor/bin/sail php vendor/bin/phpstan analyse src --level 3 >"$temp_file" 2>&1
-
-if [ $? -eq 0 ]; then
-    echo "SUCCESS"
-    rm "$temp_file"  # Clean up the temporary file on success
-else
-    echo "FAIL"
-    cat "$temp_file"  # Display the output on failure
-    rm "$temp_file"  # Clean up the temporary file
-fi
+run_command "./vendor/bin/sail php vendor/bin/phpstan analyse src --level 3"
 
 echo "##################"
 echo "# Mess Detection #"
 echo "##################"
-temp_file=$(mktemp)
-
-./vendor/bin/sail php vendor/bin/phpmd src/ text codesize,unusedcode,design,cleancode >"$temp_file" 2>&1
-
-if [ $? -eq 0 ]; then
-    echo "SUCCESS"
-    rm "$temp_file"  # Clean up the temporary file on success
-else
-    echo "FAIL"
-    cat "$temp_file"  # Display the output on failure
-    rm "$temp_file"  # Clean up the temporary file
-fi
+run_command "./vendor/bin/sail php vendor/bin/phpmd src/ text codesize,unusedcode,design,cleancode"
 
 echo "###################"
 echo "# Code Formatting #"
 echo "###################"
-temp_file=$(mktemp)
-
-./vendor/bin/sail php vendor/bin/phpcs --standard=PSR12 src/ >"$temp_file" 2>&1
-
-if [ $? -eq 0 ]; then
-    echo "SUCCESS"
-    rm "$temp_file"  # Clean up the temporary file on success
-else
-    echo "FAIL"
-    cat "$temp_file"  # Display the output on failure
-    rm "$temp_file"  # Clean up the temporary file
-fi
+run_command "./vendor/bin/sail php vendor/bin/phpcs --standard=PSR12 src/"
