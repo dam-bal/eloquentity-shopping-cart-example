16cf7b26d570357c942a919c7d1017bdf12c3ee3
dam-bal <d.balandowski@icloud.com>
2024-04-08 21:44:55 +0100

    commands/handlers for adding product to cart, removing product from cart and creating cart for customer

--- src/Shared/Application/CommandBusInterface.php
+++ src/Shared/Application/CommandBusInterface.php
@@ -6,5 +6,8 @@ interface CommandBusInterface
 {
     public function dispatch(CommandInterface $command): void;
 
+    /**
+     * @param array<class-string, class-string> $map
+     */
     public function register(array $map): void;
 }
